9ab5615ff64980a79b59946e71b281684c25a1ca
arosenb2 <aaron.rosenbaum@gmail.com>
2014-01-02 11:55:43 -0500

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -2,4 +2,36 @@ var app = angular.module("app",[]);
 
 app.controller("MainCtrl", function($scope){
 	$scope.summary = false;
+	$scope.tech = false;
+	$scope.screenshots = false;
+	$scope.resume = false;
+	
+	$scope.resetPages = function(){
+		$scope.summary = false;
+		$scope.tech = false;
+		$scope.screenshots = false;
+		$scope.resume = false;	
+	}
+	
+	$scope.showSummary = function(){
+		resetPages();
+		$scope.summary = true;
+	};
+	
+	$scope.showTech = function(){
+		resetPages();
+		$scope.tech = true;
+	};
+	
+	$scope.showScreenshots = function(){
+		resetPages();
+		$scope.screenshots = true;
+	};
+	
+	$scope.showResume = function(){
+		resetPages();
+		$scope.resume = true;
+	};
+	
+	
 });
