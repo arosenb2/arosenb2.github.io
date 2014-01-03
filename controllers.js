e3036e4ab684c1991760ec0e0cad0cb704f0d9bd
arosenb2 <aaron.rosenbaum@gmail.com>
2014-01-03 11:02:47 -0500

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -39,5 +39,25 @@ app.controller("MainCtrl", function($scope){
 		$scope.resume = true;
 	};
 	
-	
+	$scope.portfolio = [{
+		title: "CGI IQ Suite &reg; Mobile",
+		src: "//www.placekitten.com/600/400",
+		description: "CGI IQ Suite &reg; is CGI's registered project management suite. It provides high visibility of operations to team members and clients, as well as informative dashboards for management. CGI IQ Suite &reg; Mobile is currently being used by over 10 projects internally."
+	},{
+		title: "Nursing Home Advisor",
+		src: "//www.placekitten.com/g/600/400",
+		description: "Nursing Home Advisor (NHA) is an ongoing app that we expect to release in 2014 that helps individuals find the best nursing homes in the area, based on community data and surveys."
+	},{
+		title: "Vets Guide",
+		src: "//www.placekitten.com/600/400",
+		description: "This app for iPhone was used in a demo with the office of Veteran's Affairs to help provide services to those who have returned from serving their country. It showed local and regional VA clinics, food pantries, etc."
+	},{
+		title: "CGI IQ Suite &reg; Scribe",
+		src: "//www.placekitten.com/g/600/400",
+		description: "Scribe is a specialty app I'm currently working on that combines advanced note taking capabilities with the robustness of CGI IQ Suite &reg;. Features will include live writing through web sockets and synchronization with CGI IQ Suite &reg;"
+	},{
+		title: "Highlands Fellowship Small Groups",
+		src: "//www.placekitten.com/600/400",
+		description: "This is a personal project I'm working on for my church that aims to provide a social media experience for small groups, allowing them to meet online or facilitate in person meetings."
+	}];
 });
