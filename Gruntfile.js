6c1717627a2b1de259b41ab52b0ec74f01213fd2
Aaron Rosenbaum <aaron.rosenbaum@gmail.com>
2014-03-26 15:13:28 -0400

    Updated Task Runner and Reduced Bootstrap Components

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,10 +32,25 @@ module.exports = function(grunt) {
                 ]
             }
         },
+        concat: {
+            basic_and_extras: {
+                files: {
+                    'build/css/style.css' : ['build/css/bootstrap.min.css','build/css/bootstrap-theme.min.css','build/css/font-awesome.min.css'],
+                    'build/js/app.js' : ['build/js/angular.min.js','build/js/angular.sanitize.min.js','controllers.min.js']
+                }
+            }
+        },
         uglify: {
             js: {
                 files: {
-                    'build/js/controllers.min.js' : ['src/js/controllers.js']
+                    'build/js/app.min.js' : ['build/js/app.js']
+                }
+            }
+        },
+        cssmin: {
+            css: {
+                files: {
+                    'build/css/app.min.css' : ['build/css/style.css']
                 }
             }
         }
@@ -44,5 +59,5 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['concat','uglify','cssmin']);
 };
