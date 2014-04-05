cb0de1a1c3a8a9f442ea6ee7dedb3a79e76e582b
Aaron Rosenbaum <aaron.rosenbaum@gmail.com>
2014-04-05 12:21:58 -0400

    Added Tags to Projects

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -41,49 +41,49 @@ app.service("projectService",function(){
 		title: "CGI IQ Suite &reg; Mobile",
 		src: "build/img/other/650x450.png",
 		description: "CGI IQ Suite &reg; is CGI's registered project management suite. It provides high visibility of operations to team members and clients, as well as informative dashboards for management. CGI IQ Suite &reg; Mobile is currently being used by over 10 projects internally.",
-        tags:[],
+        tags:["HTML5","CSS3","Objective C","iOS","JSON","PHP","jQuery"],
         images:[]
 	},{
         id:2,
 		title: "Nursing Home Advisor",
 		src: "build/img/other/650x450.png",
 		description: "Nursing Home Advisor (NHA) is an ongoing app that we expect to release in 2014 that helps individuals find the best nursing homes in the area, based on community data and surveys.",
-        tags:[],
+        tags:["Objective C","iOS","PHP","MySQL"],
         images:[]
 	},{
         id:3,
 		title: "Vets Guide",
 		src: "build/img/other/650x450.png",
 		description: "This app for iPhone was used in a demo with the Department of Veteran's Affairs to help provide services to those who have returned from serving their country. It showed local and regional VA clinics, food pantries, etc.",
-        tags:[],
+        tags:["Objective C","iOS","PHP","MySQL"],
         images:[]
 	},{
         id:4,
 		title: "CGI IQ Suite &reg; Scribe",
 		src: "build/img/other/650x450.png",
 		description: "Scribe is a specialty app I'm currently working on that combines advanced note taking capabilities with the robustness of CGI IQ Suite &reg;. Features will include live writing through web sockets and synchronization with CGI IQ Suite &reg;",
-        tags:[],
+        tags:["HTML5","CSS3","Objective C","iOS","JSON","PHP","jQuery","MySQL"],
         images:[]
 	},{
         id:5,
 		title: "arosenb2.github.io",
 		src: "build/img/other/650x450.png",
 		description: "I designed and coded this site you are currently on to help showcase my talent.",
-        tags:[],
+        tags:["HTML5","CSS3","Responsive Web Design","AngularJS","Grunt.js","Web Design","Pagespeed Optimization"],
         images:[]
 	},{
         id:6,
 		title: "Highlands Fellowship Small Groups",
 		src: "build/img/other/650x450.png",
 		description: "This is a personal project I'm working on for my church that aims to provide a social media experience for small groups, allowing them to meet online or facilitate in person meetings.",
-        tags:[],
+        tags:["HTML5","CSS3","Responsive Web Design","AngularJS","Node.js","Passport.js","Mongoose.js","MongoDB","Web Design"],
         images:[]
 	},{
         id:7,
 		title: "Custom Building Solutions Inc.",
 		src: "build/img/other/650x450.png",
 		description: "I am currently desigining and coding a custom Wordpress theme for a local general contractor.",
-        tags:[],
+        tags:["HTML5","CSS3","Responsive Web Design","Wordpress","Web Design"],
         images:[]
 	}];
     
